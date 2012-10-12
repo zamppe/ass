7e9785ce90016a1f2c8c10b7e2b90347171ae8d5
ok <jumppe@jippii.fi>
2012-10-13 00:01:00 +0300

    okedokkkeeeee

--- scripts/game.js
+++ scripts/game.js
@@ -1,45 +1,69 @@
-/*
-Created by Slynk free to use, distribute, and alter.
-coder.slynk@gmail.com
-blog.slynksrevenge.com
-ALTERED BY ZAMPIK XDDDD))O))O)O
-*/
+function Player(x, y){
+    this.x = x;
+    this.y = y;
+    this.r = PLAYER_RADIUS;
+    this.vx = 0;
+    this.vy = 0;
+    this.angle = 0;
+    this.life = 0; 
+    this.maxLife = 0;
+}
+Player.prototype.accelerate = function(dt){
+    //orientation
+    x = this.r * Math.cos(this.angle * Math.PI / 180);
+    y = this.r * Math.sin(this.angle * Math.PI / 180);
+    //unit vector
+    ux = x / this.r;
+    uy = y / this.r;
+    //updates
+    this.vx += ux * PLAYER_ACCELERATION * dt;
+    this.vy += uy * PLAYER_ACCELERATION * dt;
+}
+Player.prototype.update = function(dt){
+    //updates
+    this.angle = 180 / Math.PI *Math.atan2(mouseY-this.y, mouseX - this.x);
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;   
+}
 
+Player.prototype.reduceLives = function(lives){
+    this.life -= 1;
+}
+
+Player.prototype.addLives = function(lives){
+    this.life += 1;
+    this.maxLife += 1;
+}
 
 var _canvas = null;
 var _buffer = null;
 var canvas = null;
 var buffer = null;
+
+isdown = {w: 0, e: 0, q: 0, space: 0}; 
+var ammos = new Array();
+var ufos = new Array();
+
 var this_frame = new Date();
 var prev_frame = new Date();
 
-isdown = {up:0,right:0,down: 0, left:0, space: 0, e: 0, q: 0}; 
-bjenn = {vx: 0, vy: 0, x: 400, y: 300, r: 50, angle: 0, lives: 0};
-
 var PLAYER_ACCELERATION = 500;
+var PLAYER_RADIUS = 50;
 var AMMO_VELOCITY = 900;
 var SHOOT_WAIT = 500;
-var waitReduced = 0;
+var AMMO_RADIUS = 5;
+var UFO_RADIUS = 25;
 
 var gameover = 0;
-var debug = 0;
 var level = 0;
 var score = 0;
-var shoot_time = -SHOOT_WAIT + this_frame.getTime();
-var ammos = new Array();
-var ammor = 5;
-var ufos = new Array();
-var ufor = 25;
-
-//ufoSpeedReduced = 1.0;
-
+var shootTime = 0;
 var mouseX = 0;
 var mouseY = 0;
-
 var turrets = 1;
-
 var kills = 0;
 var bonus = 0;
+var shootWaitReduced = 0;
 var bonusEnemySpeed = 0;
 var bonusTurret = 0;
 var bonusRof = 0;
@@ -55,7 +79,7 @@ function checkMouseMove(e){
         mouseX = e.offsetX;
         mouseY = e.offsetY;
     }
-    else if(e.layerX) {
+    else if(e.layerX){
         mouseX = e.layerX;
         mouseY = e.layerY;
     }
@@ -63,25 +87,9 @@ function checkMouseMove(e){
 
 function checkKeyDown(e) {
     var keyID = e.keyCode || e.which;
-    if (keyID === 38 || keyID === 87) { //up arrow or W key
+    if (keyID === 87) { //W 
         e.preventDefault();
-        isdown.up = 1;
-    }
-    else if (keyID === 39 || keyID === 68) { //right arrow or D key
-        e.preventDefault();
-        isdown.right = 1;
-    }
-    else if (keyID === 40 || keyID === 83) { //down arrow or S key
-        e.preventDefault();
-        isdown.down = 1;
-    }
-    else if (keyID === 37 || keyID === 65) { //left arrow or A key
-        e.preventDefault();
-        isdown.left = 1;
-    }
-    else if (keyID === 32) { //spacebar
-        e.preventDefault();
-        isdown.space = 1;
+        isdown.w = 1;
     }
     else if (keyID === 69) { //E
         e.preventDefault();
@@ -90,13 +98,16 @@ function checkKeyDown(e) {
     else if (keyID === 81) { //Q
         e.preventDefault();
         isdown.q = 1;
-    }        
+    }            
+    else if (keyID === 32) { //spacebar
+        e.preventDefault();
+        isdown.space = 1;
+    }
 }
 
 function checkKeyUp(e) {
     var keyID = e.keyCode || e.which;
-
-    if (keyID === 49){
+    if (keyID === 49){ //1
         if(bonus >= 20){
             if (turrets < 7){
                 turrets *= 2;
@@ -104,33 +115,17 @@ function checkKeyUp(e) {
             }
         }
     }    
-    else if (keyID === 50){
+    else if (keyID === 50){ //2
         if(bonus >= 20){
-            if (waitReduced < 350){
-                waitReduced += 50;
+            if (shootWaitReduced < 350){
+                shootWaitReduced += 50;
                 bonus -= 20;
             }
         }
     }
-    else if (keyID === 38 || keyID === 87) { //up arrow or W key
+    else if (keyID === 87) { //W 
         e.preventDefault();
-        isdown.up = 0;
-    }
-    else if (keyID === 39 || keyID === 68) { //right arrow or D key
-        e.preventDefault();
-        isdown.right = 0;
-    }
-    else if (keyID === 40 || keyID === 83) { //down arrow or S key
-        e.preventDefault();
-        isdown.down = 0;
-    }
-    else if (keyID === 37 || keyID === 65) { //left arrow or A key
-        e.preventDefault();
-        isdown.left = 0;
-    }
-    else if (keyID === 32) { //spacebar
-        e.preventDefault();
-        isdown.space = 0;
+        isdown.w = 0;
     }
     else if (keyID === 69) { //E
         e.preventDefault();
@@ -139,11 +134,15 @@ function checkKeyUp(e) {
     else if (keyID === 81) { //Q
         e.preventDefault();
         isdown.q = 0;
-    }         
+    }     
+    else if (keyID === 32) { //spacebar
+        e.preventDefault();
+        isdown.space = 0;
+    }
 }
 
 function addUfo(x1, y1, v, life1){
-    ufos.push({x:x1, y:y1, maxv: v, maxlife: Math.max(life1, 1), life: Math.max(life1, 1)});
+    ufos.push({x:x1, y:y1, maxv: v, maxLife: Math.max(life1, 1), life: Math.max(life1, 1)});
 }
 
 function removeUfo(i){
@@ -155,23 +154,22 @@ function removeUfo(i){
 
 function nextLevel(){
     level += 1;
-    bjenn.lives += 1;
     for(i=0; i<level; i++){
         r = Math.floor(Math.random() * 4);
         if(r == 0){
             x = Math.floor(Math.random() * 800);
-            y = ufor;
+            y = UFO_RADIUS;
         }
         if(r == 1){
             x = Math.floor(Math.random() * 800);
-            y = 600 - ufor;
+            y = 600 - UFO_RADIUS;
         }
         if(r == 2){
-            x = 800 - ufor;
+            x = 800 - UFO_RADIUS;
             y = Math.floor(Math.random() * 600);
         }
         if(r == 3){
-            x = ufor;
+            x = UFO_RADIUS;
             y = Math.floor(Math.random() * 600);
         }        
         addUfo(x, y, 5 * level, Math.floor(level / 3));
@@ -228,11 +226,24 @@ function generateLightning(startX, startY, endX, endY, iterations){
     */
     return segments;
 } 
+
+function drawHealthBar(ctx, x, y, r, maxLife, life, width, height){
+    ctx.fillStyle = "rgb(255,0,0)";
+    ctx.beginPath();
+    ctx.fillRect(x - width / 2, y - r - height * 2, width, height);
+    ctx.closePath();
+    
+    ctx.fillStyle = "rgb(0,255,0)";
+    ctx.beginPath();
+    ctx.fillRect(x - width / 2, y - r - height * 2, life * width / maxLife, height);
+    ctx.closePath();  
+}
     
 function Game(){
 	this.gameLoop = null;
 	var self = this;
 	var lightning = new Array();
+    var bjenn = new Player(400, 300);
     var lightningTime = 0;
     var lightningAge = 0;
 	this.Init = function(){
@@ -262,21 +273,14 @@ function Game(){
         this_frame = new Date();
         dt = (this_frame.getTime() - prev_frame.getTime()) / 1000;
         
-		if(isdown.up){
-            x = bjenn.x - bjenn.x + bjenn.r * Math.cos(bjenn.angle * Math.PI / 180);
-            y = bjenn.y - bjenn.y + bjenn.r * Math.sin(bjenn.angle * Math.PI / 180);
-            normik = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-            ux = x / normik;
-            uy = y / normik;
-            bjenn.vx += ux * PLAYER_ACCELERATION * dt;
-            bjenn.vy += uy * PLAYER_ACCELERATION * dt;
+		if(isdown.w){
+            bjenn.accelerate(dt);
         }   
+        bjenn.update(dt);
 
-        bjenn.angle = 180 / Math.PI *Math.atan2(mouseY-bjenn.y, mouseX - bjenn.x);
-        bjenn.x += bjenn.vx * dt;
-        bjenn.y += bjenn.vy * dt;   
+
         if(isdown.e){
-            if(this_frame.getTime() - shoot_time > SHOOT_WAIT - waitReduced){
+            if(this_frame.getTime() - shootTime > SHOOT_WAIT - shootWaitReduced){
                 for(i = 1; i<= turrets; i++){
                     turretStep = turretsWidth/turrets;
                     x = bjenn.x - bjenn.x + bjenn.r * Math.cos(((bjenn.angle + turretStep * i) - turretsWidth ) * Math.PI / 180);
@@ -286,18 +290,18 @@ function Game(){
                     uy = y / normik;
                     vx = ux * AMMO_VELOCITY;
                     vy = uy * AMMO_VELOCITY;
-                    ammos.push({vy: vy, vx: vx, r: ammor, y: bjenn.y + bjenn.r * Math.sin(((bjenn.angle + turretStep * i) - turretsWidth ) * Math.PI / 180), x: bjenn.x + bjenn.r * Math.cos(((bjenn.angle + turretStep * i) - turretsWidth) * Math.PI / 180)});                
+                    ammos.push({vy: vy, vx: vx, r: AMMO_RADIUS, y: bjenn.y + bjenn.r * Math.sin(((bjenn.angle + turretStep * i) - turretsWidth ) * Math.PI / 180), x: bjenn.x + bjenn.r * Math.cos(((bjenn.angle + turretStep * i) - turretsWidth) * Math.PI / 180)});                
                 }
-                shoot_time = this_frame.getTime();
+                shootTime = this_frame.getTime();
             }                      
         }
         
         if(isdown.q){
-            if(this_frame.getTime() - shoot_time > SHOOT_WAIT - waitReduced){
+            if(this_frame.getTime() - shootTime > SHOOT_WAIT - shootWaitReduced){
                 //var length = Math.sqrt(Math.pow(bjenn.x - mouseX, 2) + Math.pow(bjenn.y - mouseY, 2));
                 lightning = generateLightning(bjenn.x, bjenn.y, mouseX, mouseY, 7);
                 lightningTime = this_frame.getTime();
-                shoot_time = this_frame.getTime();
+                shootTime = this_frame.getTime();
             }
         }
         lightningAge = this_frame.getTime() - lightningTime;
@@ -308,6 +312,7 @@ function Game(){
         if(ufos.length == 0)
         {   
             nextLevel();
+            bjenn.addLives(1);
         }        
 
         
@@ -321,7 +326,7 @@ function Game(){
         for(j = 0; j < ufos.length; j++){   
             var remove_ammos = new Array();
             for(i = 0; i < ammos.length; i++){
-                if(circle_circle_collision(ammos[i].x, ammos[i].y, ammos[i].r, ufos[j].x, ufos[j].y, ufor)){
+                if(circle_circle_collision(ammos[i].x, ammos[i].y, ammos[i].r, ufos[j].x, ufos[j].y, UFO_RADIUS)){
                     ufos[j].life -= 1;
                     remove_ammos.push(i);
                     if(ufos[j].life == 0){
@@ -336,7 +341,7 @@ function Game(){
         var done = 0;
         for(i = 0; i < lightning.length; i++){
             for(j = 0; j < ufos.length; j++){
-                if(circle_circle_collision(lightning[i].x1, lightning[i].y1, 0, ufos[j].x, ufos[j].y, ufor)){            
+                if(circle_circle_collision(lightning[i].x1, lightning[i].y1, 0, ufos[j].x, ufos[j].y, UFO_RADIUS)){            
                     done = 1;
                     break;
                 }
@@ -358,12 +363,12 @@ function Game(){
             vy = uy * ufos[i].maxv;
             ufos[i].x += vx * dt;
             ufos[i].y += vy * dt;
-            if(circle_circle_collision(ufos[i].x, ufos[i].y, ufor, bjenn.x, bjenn.y, bjenn.r)){
+            if(circle_circle_collision(ufos[i].x, ufos[i].y, UFO_RADIUS, bjenn.x, bjenn.y, bjenn.r)){
                 ufos.splice(i, 1);
-                bjenn.lives -= 1; 
+                bjenn.reduceLives(1); 
             }
         }
-        if(bjenn.lives < 1){
+        if(bjenn.life < 1){
             gameover = 1;
         }
 	}
@@ -381,23 +386,15 @@ function Game(){
         
         //line inside player circle
         buffer.beginPath();
-        buffer.lineCap="round";
+        buffer.lineCap = "round";
         buffer.moveTo(bjenn.x , bjenn.y);
         buffer.lineTo(bjenn.x + bjenn.r * Math.cos(bjenn.angle * Math.PI / 180), bjenn.y + bjenn.r * Math.sin(bjenn.angle * Math.PI / 180));
         buffer.stroke();         
         buffer.closePath();   
         
         //player healthbar
-        buffer.fillStyle = "rgb(255,0,0)";
-        buffer.beginPath();
-        buffer.fillRect(bjenn.x-35, bjenn.y-bjenn.r-12, 70, 8);
-        buffer.closePath();
-        
-        buffer.fillStyle = "rgb(0,255,0)";
-        buffer.beginPath();
-        var step = 70 / level;
-        buffer.fillRect(bjenn.x-35, bjenn.y-bjenn.r-12, bjenn.lives*step, 8);
-        buffer.closePath();     
+        drawHealthBar(buffer, bjenn.x, bjenn.y, bjenn.r, bjenn.maxLife, bjenn.life, 70, 6);
+   
         
         //ammos
         for(var i = 0; i < ammos.length; i++){
@@ -426,25 +423,16 @@ function Game(){
         buffer.lineWidth = 1;
         
         //jufos
-        for(var i = 0; i < ufos.length; i++){
-            var step = 30/(ufos[i].maxlife);    
+        for(var i = 0; i < ufos.length; i++){   
             //circle
             buffer.fillStyle = "rgb(127, 0, 255)";
             buffer.beginPath();
-            buffer.arc(ufos[i].x, ufos[i].y, ufor, 0, Math.PI*2, true);            
+            buffer.arc(ufos[i].x, ufos[i].y, UFO_RADIUS, 0, Math.PI*2, true);            
             buffer.fill();
             buffer.closePath();
             
             //healthbars
-            buffer.fillStyle = "rgb(255,0,0)";
-            buffer.beginPath();
-            buffer.fillRect(ufos[i].x-15, ufos[i].y-ufor-6, 30, 4);
-            buffer.closePath();
-            
-            buffer.fillStyle = "rgb(0,255,0)";
-            buffer.beginPath();
-            buffer.fillRect(ufos[i].x-15, ufos[i].y-ufor-6, ufos[i].life*step, 4);
-            buffer.closePath();   
+            drawHealthBar(buffer, ufos[i].x, ufos[i].y, UFO_RADIUS, ufos[i].maxLife, ufos[i].life, 70, 6);
         }   
         
         //text
@@ -453,7 +441,7 @@ function Game(){
         buffer.fillText("scoor : "+ score, 78, 16);
         buffer.fillText("bonus available: "+ Math.floor(bonus/20), 228, 16);
         buffer.fillText("turrets: "+ turrets, 528, 16);
-        buffer.fillText("rate of fire: "+ ((SHOOT_WAIT-waitReduced)/1000), 628, 16);        
+        buffer.fillText("rate of fire: "+ ((SHOOT_WAIT-shootWaitReduced)/1000), 628, 16);        
 		buffer.fillStyle = "rgb(255, 255, 255)";
 
 		canvas.drawImage(_buffer, 0, 0);
